fa075c61c4cb8962818ceb40376c351ffce5ff43
Linux <adarsh00502@gmail.com>
2022-09-16 19:18:50 +0530

    api

--- src/App.js
+++ src/App.js
@@ -11,26 +11,20 @@ const [el, setel]=useState({
 
   function makereq(q){
 
-    // var options = {
-    //   method: 'GET',
-    //   url: 'https://api.newscatcherapi.com/v2/search',
-    //   params: {q: q, lang: 'en', sort_by: 'relevancy', page: '1'},
-    //   headers: {
-    //     'x-api-key': 'U62XEMG_finzHFoYUYGPboSfbxpjb6abCLSNUzOL24c'
-    //   }
-    // };
+    var options = {
+      method: 'GET',
+      url: 'https://api.newscatcherapi.com/v2/search',
+      params: {q: q, lang: 'en', sort_by: 'relevancy', page: '1'},
+      headers: {
+        'x-api-key': 'U62XEMG_finzHFoYUYGPboSfbxpjb6abCLSNUzOL24c'
+      }
+    };
 
-    // axios.request(options).then(function (response) {
-    //   console.log(response.data);
-    // }).catch(function (error) {
-    //   console.error(error);
-    // });
-
-    axios.get('https://newsapi.org/v2/everything?q=tesla&from=2022-08-16&sortBy=publishedAt&apiKey=a9fef14665c84227a2c932aa86e7a14e')
-    .then((res)=>console.log(res.data.articles))
-    .catch((err)=>console.log(err))
-
-    
+    axios.request(options).then(function (response) {
+      console.log(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
   }
 
   return (
