b9020dc61439edf3fc4eb875b141fdf14cefc51f
Linux <adarsh00502@gmail.com>
2022-09-16 19:16:16 +0530

    testing api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import './App.css';
 import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
 
-  var options = {
-    method: 'GET',
-    url: 'https://api.newscatcherapi.com/v2/search',
-    params: {q: 'Bitcoin', lang: 'en', sort_by: 'relevancy', page: '1'},
-    headers: {
-      'x-api-key': 'U62XEMG_finzHFoYUYGPboSfbxpjb6abCLSNUzOL24c'
-    }
-  };
+// eslint-disable-next-line no-unused-vars
+const [el, setel]=useState({
+  q:"Australia"
+});
 
-  axios.request(options).then(function (response) {
-    console.log(response.data);
-  }).catch(function (error) {
-    console.error(error);
-  });
+  function makereq(q){
+
+    // var options = {
+    //   method: 'GET',
+    //   url: 'https://api.newscatcherapi.com/v2/search',
+    //   params: {q: q, lang: 'en', sort_by: 'relevancy', page: '1'},
+    //   headers: {
+    //     'x-api-key': 'U62XEMG_finzHFoYUYGPboSfbxpjb6abCLSNUzOL24c'
+    //   }
+    // };
+
+    // axios.request(options).then(function (response) {
+    //   console.log(response.data);
+    // }).catch(function (error) {
+    //   console.error(error);
+    // });
+
+    axios.get('https://newsapi.org/v2/everything?q=tesla&from=2022-08-16&sortBy=publishedAt&apiKey=a9fef14665c84227a2c932aa86e7a14e')
+    .then((res)=>console.log(res.data.articles))
+    .catch((err)=>console.log(err))
+
+    
+  }
 
   return (
     <div className="App">
       hhe
+      <button onClick={()=>{makereq(el.q)}}>click</button>
 
     </div>
   );
